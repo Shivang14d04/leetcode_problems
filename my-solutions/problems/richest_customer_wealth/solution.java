dfb97a00268e02126eb88c4720ea9507404240bf
Shivang <kshivang679@gmail.com>
2024-09-12 17:55:16 +0000

    Sync LeetCode submission - Richest Customer Wealth (java)

--- my-solutions/problems/richest_customer_wealth/solution.java
+++ my-solutions/problems/richest_customer_wealth/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int ans = 0;
-        for(int i = 0;i<accounts.length;i++){
-            int sum = 0;
-            for(int j = 0;j<accounts[i].length;j++){
-            sum += accounts[i][j];
-            }
-            if(sum > ans){
-                ans = sum;
-            }
-
+        int sum =0;
+        int max = 0;
+      for(int i =0;i<accounts.length;i++){
+        for(int j=0;j<accounts[i].length;j++){
+            sum = sum+accounts[i][j];
         }
-        return ans;
-
+        if(sum > max){
+            max = sum;
         }
-
+        sum =0;
+      } 
+      return max; 
     }
+}
