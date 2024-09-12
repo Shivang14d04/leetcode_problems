6c16a3cb6e8cff904968b74d658a5b4acad9251d
Shivang <kshivang679@gmail.com>
2024-09-12 18:33:27 +0000

    Sync LeetCode submission - Shuffle the Array (java)

--- my-solutions/problems/shuffle_the_array/solution.java
+++ my-solutions/problems/shuffle_the_array/solution.java
@@ -1,10 +1,12 @@
+// Approach -1
+
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int [] arr = new int[2*n];
-        for(int i = 0;i<n;i++){
-            arr[2*i] = nums[i];
-            arr[2*i+1] = nums[i+n];
+        int[] result = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[i + n];
         }
-        return arr;
+        return result;
     }
 }
