55c656f7ed6b8e47b31a5ca9fbcc55b938dd495e
Shivang <kshivang679@gmail.com>
2024-09-23 15:03:32 +0000

    Sync LeetCode submission - Minimum Cost to Move Chips to The Same Position (java)

--- my-solutions/problems/minimum_cost_to_move_chips_to_the_same_position/solution.java
+++ my-solutions/problems/minimum_cost_to_move_chips_to_the_same_position/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
-     int e =0, o=0;
-     for(int i : position){
-        if(i%2==0){
-            e++;
+        int even =0;
+        int odd = 0;
+        for(int i =0;i<position.length;i++){
+            if(position[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
         }
-        else {
-            o++;
-        }
-     }
-     return Math.min(e,o);   
+        return Math.min(even,odd);
     }
 }
