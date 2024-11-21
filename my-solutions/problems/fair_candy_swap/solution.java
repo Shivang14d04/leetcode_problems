04f207dfcd6880083d83eeb9a9834b6ac1742b0c
Shivang <kshivang679@gmail.com>
2024-11-21 05:17:12 +0000

    Sync LeetCode submission - Fair Candy Swap (java)

---
+++ my-solutions/problems/fair_candy_swap/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+      int sum1 =0;
+      int sum2 = 0;
+      for(int candies : aliceSizes) sum1 += candies;
+      for(int candies : bobSizes) sum2 +=candies;
+      int n = aliceSizes.length , m = bobSizes.length;
+      for(int i =0;i<n;i++){
+        for(int j =0;j<m;j++){
+            if((sum1 - aliceSizes[i] +bobSizes[j])  == (sum2 - bobSizes[j] +aliceSizes[i])){
+                return new int[] {aliceSizes[i] , bobSizes[j]};
+            }
+        }
+      }
+      return new int [0];  
+    }
+}
