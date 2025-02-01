116ad3e36de6bafa7aaf2397e679bc26bb81d947
Shivang <kshivang679@gmail.com>
2025-02-01 20:21:13 +0000

    Sync LeetCode submission - Decrypt String from Alphabet to Integer Mapping (java)

---
+++ my-solutions/problems/decrypt_string_from_alphabet_to_integer_mapping/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(int i =s.length()-1;i>=0;i--){
+            int num;
+            if(s.charAt(i)=='#'){
+                num  = (s.charAt(i-2)-'0') *10 + (s.charAt(i-1)-'0');
+                i-=2;
+            }
+            else{
+                num = s.charAt(i)-'0';
+            }
+            ans.append((char)( num+96));
+        }
+        return ans.reverse().toString();
+        
+    }
+}
