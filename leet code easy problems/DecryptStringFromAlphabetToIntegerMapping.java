974ba4091510a1e46add3674ac4d24cf8723c9e3
Shivang14d04 <kshivang679@gmail.com>
2025-02-02 01:57:28 +0530

    leetcode problem

--- leet code easy problems/DecryptStringFromAlphabetToIntegerMapping.java
+++ leet code easy problems/DecryptStringFromAlphabetToIntegerMapping.java
@@ -1,18 +1,19 @@
-public class DecryptStringFromAlphabetToIntegerMapping{
+
+public class DecryptStringFromAlphabetToIntegerMapping {
+
     public static void main(String[] args) {
-        String s ="10#11#12";
+        String s = "10#11#12";
         StringBuilder ans = new StringBuilder();
-        for(int i =s.length()-1;i>=0;i--){
+        for (int i = s.length() - 1; i >= 0; i--) {
             int num;
-            if(s.charAt(i)=='#'){
-                num  = (s.charAt(i-2)-'0') *10 + (s.charAt(i-1)-'0');
-                i-=2;
+            if (s.charAt(i) == '#') {
+                num = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+                i -= 2;
+            } else {
+                num = s.charAt(i) - '0';
             }
-            else{
-                num = s.charAt(i)-'0';
-            }
-            ans.append((char)( num+96));
+            ans.append((char) (num + 96));
         }
-        System.out.println(ans.reverse().toString());        
+        System.out.println(ans.reverse().toString());
     }
 }
