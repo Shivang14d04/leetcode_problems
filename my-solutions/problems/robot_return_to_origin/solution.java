b6e7a939f4023990ee6b9cefe6c0087c1ead1303
Shivang <kshivang679@gmail.com>
2025-02-02 19:11:16 +0000

    Sync LeetCode submission - Robot Return to Origin (java)

---
+++ my-solutions/problems/robot_return_to_origin/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        char [] ans = moves.toCharArray();
+        int cnt_U =0;
+        int cnt_D =0;
+        int cnt_L = 0;
+        int cnt_R = 0;
+        if(moves.length()%2!=0){
+            return false;
+        }
+        else{
+            for(int i = 0;i< ans.length;i++){
+                if(ans[i]=='D'){
+                    cnt_D++;
+                }
+                else if(ans[i]=='U'){
+                    cnt_U++;
+                }
+                else if(ans[i]=='L'){
+                    cnt_L++;
+                }
+                else{
+                    cnt_R++;
+                }  
+            }
+
+        }
+        if(cnt_U == cnt_D && cnt_L==cnt_R){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
