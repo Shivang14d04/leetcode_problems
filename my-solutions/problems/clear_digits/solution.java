8543ce63189368418eff5f8bc2b27da22449cb50
Shivang <kshivang679@gmail.com>
2025-02-10 17:52:21 +0000

    Sync LeetCode submission - Clear Digits (java)

---
+++ my-solutions/problems/clear_digits/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String clearDigits(String s) {
+      StringBuilder b = new StringBuilder(s.toLowerCase());
+        for(int i = 1; i <b.length();i++){
+            if(b.charAt(i) <= 57 && b.charAt(i) >=48){
+                if(b.charAt(i-1)<=122 && b.charAt(i-1)>=97){
+                       b.deleteCharAt(i-1);
+                       b.deleteCharAt(i-1);
+                       i-=2;
+                }
+                else{
+                   b.deleteCharAt(i);
+                   i--;
+                }
+            }
+   
+        }
+        return b.toString();
+        
+    }
+}
