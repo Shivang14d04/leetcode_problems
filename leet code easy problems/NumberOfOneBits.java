602b56df7a664c91444a449d942e4737f86e6b63
Shivang14d04 <kshivang679@gmail.com>
2025-02-25 03:05:39 +0530

    leetcode problem

--- leet code easy problems/NumberOfOneBits.java
+++ leet code easy problems/NumberOfOneBits.java
@@ -27,4 +27,14 @@ public class NumberOfOneBits {
         }
         return count;
     }
+
+// ALTERNATE SOLUTION 
+    // public static int solution2(int n){
+    //     int count = 0;
+    //     while(n>0){
+    //         count += n&1;
+    //         n>>=1;
+    //     }
+    //     return count;
+    // }
 }
