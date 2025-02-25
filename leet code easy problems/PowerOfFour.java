1aa0978bb988affd5a3460fb1f853528c324070e
Shivang14d04 <kshivang679@gmail.com>
2025-02-26 02:12:02 +0530

    leetcode problem

---
+++ leet code easy problems/PowerOfFour.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class PowerOfFour {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(solution1(n));
+    }
+
+    public static boolean solution1(int n) {
+        if (n == 1) {
+            return true;
+        }
+        for (int i = 0; i <= 15; i++) {
+            int a = (int) Math.pow(4, i);
+            if (a == n) {
+                return true;
+            }
+            if (a > n) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static boolean solution2(int n) {
+        if (n == 1) {
+            return true;
+        }
+
+        if (n <= 0) {
+            return false;
+        }
+
+        double logbase4 = Math.log(n) / Math.log(4);
+        return (logbase4 == (int) logbase4);
+
+    }
+}
