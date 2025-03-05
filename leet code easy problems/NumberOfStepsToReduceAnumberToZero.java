079f28dbcbe83399756c657419473b972d278e1c
Shivang14d04 <kshivang679@gmail.com>
2025-03-06 01:28:29 +0530

    added recursive solution

--- leet code easy problems/NumberOfStepsToReduceAnumberToZero.java
+++ leet code easy problems/NumberOfStepsToReduceAnumberToZero.java
@@ -6,7 +6,8 @@ public class NumberOfStepsToReduceAnumberToZero {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        System.out.println(solution(n));
+        // System.out.println(solution(n));
+        System.out.println(recursiveCount(n));
     }
 
     public static int solution(int n) {
@@ -21,5 +22,19 @@ public class NumberOfStepsToReduceAnumberToZero {
             }
         }
         return count;
+    }
+
+    public static int recursiveCount(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num % 2 == 0) {
+
+            return 1 + recursiveCount(num / 2);
+
+        } else {
+
+            return 1 + recursiveCount(num - 1);
+        }
     }
 }
