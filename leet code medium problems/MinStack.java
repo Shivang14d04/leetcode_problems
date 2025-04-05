10020bb3c41ceb068d44e9f516066ab16259c8ed
Shivang14d04 <kshivang679@gmail.com>
2025-04-05 18:07:18 +0530

    leetcode problem

--- leet code medium problems/MinStack.java
+++ leet code medium problems/MinStack.java
@@ -1,45 +1,44 @@
+
+import java.util.*;
+
 public class MinStack {
+
     private Stack<Integer> stack;
 
     private Stack<Integer> min;
-
 
     public MinStack() {
         stack = new Stack<>();
         min = new Stack<>();
 
-        
     }
-    
+
     public void push(int val) {
         stack.push(val);
-        if(min.isEmpty() || val <= min.peek()){
+        if (min.isEmpty() || val <= min.peek()) {
             min.push(val);
         }
-        
+
     }
-    
+
     public void pop() {
         int popped = stack.pop();
-        if(popped == min.peek()){
+        if (popped == min.peek()) {
             min.pop();
         }
     }
-    
+
     public int top() {
         return stack.peek();
     }
-    
+
     public int getMin() {
         return min.peek();
     }
 }
 
 /**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
+ * Your MinStack object will be instantiated and called as such: MinStack obj =
+ * new MinStack(); obj.push(val); obj.pop(); int param_3 = obj.top(); int
+ * param_4 = obj.getMin();
  */
