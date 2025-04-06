4b40b15df3a4123ce75ef726dc63a658d80be237
Shivang <kshivang679@gmail.com>
2025-04-06 05:21:28 +0000

    Sync LeetCode submission - Remove Outermost Parentheses (java)

---
+++ my-solutions/problems/remove_outermost_parentheses/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(ch =='('){
+                if(!stack.isEmpty()){
+                    result.append(ch);
+                }
+                stack.push(ch);
+
+            }
+            else if(ch == ')'){
+                stack.pop();
+                if(!stack.isEmpty()){
+                    result.append(ch);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
