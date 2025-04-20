36df263b9c70d9dd1d282f87cbd5dcfa059ad328
Shivang <kshivang679@gmail.com>
2025-04-20 05:51:37 +0000

    Sync LeetCode submission - Power of Four (java)

--- my-solutions/problems/power_of_four/solution.java
+++ my-solutions/problems/power_of_four/solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1) return true;
-
-        if(n<=0) return false;
-
-        double logbase4 = Math.log(n)/Math.log(4);
-        return (logbase4 == (int)logbase4);
-        
+       if(n==1) return true;
+       if(n==0) return false;
+       if(n%4!= 0) return false;
+       return isPowerOfFour(n/4); 
     }
 }
