62383f80cbfca2114e7fb9b95e8e72442964b018
Shivang <kshivang679@gmail.com>
2025-06-02 06:38:29 +0000

    Sync LeetCode submission - Happy Number (java)

--- my-solutions/problems/happy_number/solution.java
+++ my-solutions/problems/happy_number/solution.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> seen = new HashSet<>();
-        while (n != 1) {
-            if (seen.contains(n)) {
-                return false;
-            }
-            seen.add(n);
-            n = squareSum(n);
+        int slow = n;
+        int fast = n;
+        do{
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
         }
-        return n == 1;
-        
+        while(slow!=fast);
+        if(slow==1)
+        {
+            return true;
+        }
+        return false;
     }
-    public static int squareSum(int n ){
-        int sum  = 0;
+    private int squareSum(int n ){
+        int ans =0;
         while(n>0){
-            int r = n%10;
-            sum += r*r;
-
+            int rem = n%10;
+            ans += rem*rem;
             n/=10;
         }
-        
-        return sum;
+        return ans;
     }
 }
