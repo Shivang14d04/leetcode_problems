773079ab68130d0f278db7cf9ee375d293315e05
Shivang <kshivang679@gmail.com>
2025-06-06 12:38:57 +0000

    Sync LeetCode submission - Linked List Cycle (java)

--- my-solutions/problems/linked_list_cycle/solution.java
+++ my-solutions/problems/linked_list_cycle/solution.java
@@ -11,12 +11,12 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next!= null){
-            fast = fast.next.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
-            if(slow == fast){
+            fast = fast.next.next;
+            if(fast == slow){
                 return true;
             }
         }
