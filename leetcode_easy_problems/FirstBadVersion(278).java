e363b0b7b1e4b44b1f09b1cb67dee297605baa29
Shivang <kshivang679@gmail.com>
2025-06-28 23:46:50 +0530

    pushing everthig again due to some error

--- leetcode_easy_problems/FirstBadVersion(278).java
+++ leetcode_easy_problems/FirstBadVersion(278).java
@@ -1,18 +1,1 @@
 
-    public int firstBadVersion(int n) {
-        int s = 1;
-        int e = n;
-        
-        while(s<e){
-             int mid = s + (e-s)/2;
-            if(isBadVersion(mid)==true){
-                e = mid -1;
-            }
-            else if(isBadVersion(mid)==false) {
-                s = mid+1;
-            }
-            
-        }
-        return s;
-    
-    }
