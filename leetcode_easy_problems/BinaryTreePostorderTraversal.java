c9aab09277c2a43ccd3a5167ab9f741db978c2f6
Shivang <kshivang679@gmail.com>
2025-07-01 17:51:59 +0530

    added another solution by using two stacks

--- leetcode_easy_problems/BinaryTreePostorderTraversal.java
+++ leetcode_easy_problems/BinaryTreePostorderTraversal.java
@@ -23,22 +23,47 @@ public class BinaryTreePostorderTraversal {
         }
     }
 
-    class Solution {
-
-        public List<Integer> postorderTraversal(TreeNode root) {
-            List<Integer> list = new ArrayList<>();
-            postorder(root, list);
-            return list;
-        }
-
-        public void postorder(TreeNode node, List<Integer> list) {
-            if (node == null) {
-                return;
-            }
-            postorder(node.left, list);
-            postorder(node.right, list);
-
-            list.add(node.val);
-        }
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorder(root, list);
+        return list;
     }
+
+    public void postorder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left, list);
+        postorder(node.right, list);
+
+        list.add(node.val);
+    }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty()) {
+            root = stack1.pop();
+            stack2.push(root);
+            if (root.left != null) {
+                stack1.push(root.left);
+            }
+            if (root.right != null) {
+                stack1.push(root.right);
+            }
+
+        }
+        while (!stack2.isEmpty()) {
+            ans.add(stack2.pop().val);
+        }
+        return ans;
+
+    }
+
 }
