ccf9441352cd76bc2c404181071ffd82941cb158
Shivang <kshivang679@gmail.com>
2025-07-05 12:23:54 +0530

    Solved using breadth-first-search in O(n) time complexity

---
+++ leetcode_medium_problems/BinaryTreeRightSideView.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+public class BinaryTreeRightSideView {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            if (root == null) {
+                return list;
+            }
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+                list.add(((LinkedList<TreeNode>) queue).getLast().val);
+                for (int i = 0; i < levelSize; i++) {
+                    TreeNode current = queue.poll();
+
+                    if (current.left != null) {
+                        queue.offer(current.left);
+                    }
+                    if (current.right != null) {
+                        queue.offer(current.right);
+                    }
+                }
+
+            }
+            return list;
+        }
+    }
+}
