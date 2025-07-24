e8a29fdcbedeec2884e5232288b1e34d1c50a72b
Shivang <kshivang679@gmail.com>
2025-07-24 23:42:27 +0530

    added another solution

--- leetcode_medium_problems/CombinationSum.java
+++ leetcode_medium_problems/CombinationSum.java
@@ -13,12 +13,12 @@ public class CombinationSum {
         }
         ArrayList<Integer> p = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-        CombinationSum(p, candidates, target, result, 0);
+        CombinationSum1(p, candidates, target, result, 0);
         System.out.println(result);
 
     }
 
-    public static void CombinationSum(ArrayList<Integer> p, int[] candidates, int sum, List<List<Integer>> result, int index) {
+    public static void CombinationSum1(ArrayList<Integer> p, int[] candidates, int sum, List<List<Integer>> result, int index) {
         if (sum == 0) {
             result.add(new ArrayList<>(p)); // Add a copy of current list
             return;
@@ -30,8 +30,32 @@ public class CombinationSum {
 
         for (int i = index; i < candidates.length; i++) {
             p.add(candidates[i]);
-            CombinationSum(p, candidates, sum - candidates[i], result, i); // allow reuse
+            CombinationSum1(p, candidates, sum - candidates[i], result, i); // allow reuse
             p.remove(p.size() - 1); // backtrack
+        }
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, target, ans, result, 0, 0);
+        return result;
+
+    }
+
+    public static void helper(int[] arr, int target, List<Integer> ans, List<List<Integer>> result, int sum, int index) {
+        if (sum == target) {
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+
+        for (int i = index; i < arr.length; i++) {
+            ans.add(arr[i]);
+            helper(arr, target, ans, result, sum + arr[i], i);
+            ans.remove(ans.size() - 1);
         }
     }
 }
