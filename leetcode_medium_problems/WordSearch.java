a4893bab8c6e10cea38f9965c95f39fcd9920dc1
Shivang <kshivang679@gmail.com>
2025-07-28 02:18:42 +0530

    solved using backtracking in O(n*m*4^k) time complexity

--- leetcode_medium_problems/WordSearch.java
+++ leetcode_medium_problems/WordSearch.java
@@ -2,13 +2,13 @@
 public class WordSearch {
 
     public boolean exist(char[][] board, String word) {
-
         int n = board.length;
         int m = board[0].length;
         boolean[][] visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (solution("", board, word, i, j, visited)) {
+                if (helper(board, word, 0, i, j, visited)) {
                     return true;
                 }
             }
@@ -17,25 +17,22 @@ public class WordSearch {
 
     }
 
-    public boolean solution(String p, char[][] board, String word, int i, int j, boolean[][] visited) {
-        if (word.length() == 0) {
+    public boolean helper(char[][] board, String word, int index, int i, int j, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
         }
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(0) != board[i][j]) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
 
         visited[i][j] = true;
-        word = word.substring(1);
 
-        boolean found = solution(p + board[i][j], board, word, i + 1, j, visited)
-                || solution(p + board[i][j], board, word, i, j + 1, visited)
-                || solution(p + board[i][j], board, word, i - 1, j, visited)
-                || solution(p + board[i][j], board, word, i, j - 1, visited);
+        Boolean found = helper(board, word, index + 1, i + 1, j, visited)
+                || helper(board, word, index + 1, i - 1, j, visited)
+                || helper(board, word, index + 1, i, j + 1, visited)
+                || helper(board, word, index + 1, i, j - 1, visited);
 
         visited[i][j] = false;
         return found;
-
     }
-
 }
