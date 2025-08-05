a0151cbcbb6b7a42d3d099290ab7b74107ae9333
Shivang <kshivang679@gmail.com>
2025-08-06 02:16:44 +0530

    solved using tabulation

--- leetcode_medium_problems/UniquePaths.java
+++ leetcode_medium_problems/UniquePaths.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 
 public class UniquePaths {
 
+    // Time Complexity: O(m * n)
+// Space Complexity: O(m * n) + O(m + n) for recursion stack
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
         for (int[] rows : dp) {
@@ -31,7 +33,22 @@ public class UniquePaths {
         dp[m][n] = a + b;
         return dp[m][n];
     }
-}
 
-// Time Complexity: O(m * n)
-// Space Complexity: O(m * n) + O(m + n) for recursion stack
+    // Time Complexity: O(m * n)
+    // Space Complexity: O(m * n)
+    public int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        dp[1][1] = 1;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
+
+    }
+}
