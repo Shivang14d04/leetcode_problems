1223547c965c0ed1d7d3cf52c98af98db7578d7a
Shivang <kshivang679@gmail.com>
2025-08-07 20:59:56 +0000

    Sync LeetCode submission - Minimum Path Sum (java)

---
+++ my-solutions/problems/minimum_path_sum/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+
+        return helper(grid, dp, m - 1, n - 1);
+
+    }
+
+    public int helper(int[][] grid, int[][] dp, int i, int j) {
+        if (i == 0 && j == 0) {
+            return grid[0][0];
+        }
+        if (i < 0 || j < 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int up = helper(grid, dp, i - 1, j);
+        int left = helper(grid, dp, i, j - 1);
+        int minPath = Math.min(up, left);
+        if (minPath == Integer.MAX_VALUE) {
+            dp[i][j] = Integer.MAX_VALUE;
+        } else {
+            dp[i][j] = grid[i][j] + minPath;
+        }
+        return dp[i][j];
+    }
+}
