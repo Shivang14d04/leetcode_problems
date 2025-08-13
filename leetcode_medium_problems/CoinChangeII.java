a5837c411b779ff902233612e84e96fd56026d98
Shivang <kshivang679@gmail.com>
2025-08-14 01:34:10 +0530

    added space optimized solution

--- leetcode_medium_problems/CoinChangeII.java
+++ leetcode_medium_problems/CoinChangeII.java
@@ -74,4 +74,37 @@ public class CoinChangeII {
         return dp[n - 1][amount];
 
     }
+
+    // Space optimized approach
+    // Time complexity: O(n * amount)
+    // Space complexity: O(amount)
+    public int change3(int amount, int[] coins) {
+
+        int n = coins.length;
+        int[] prev = new int[amount + 1];
+
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                prev[i] = 1;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            int[] curr = new int[amount + 1];
+            for (int target = 0; target <= amount; target++) {
+                int skip = prev[target];
+                int take = 0;
+
+                if (coins[i] <= target) {
+                    take = curr[target - coins[i]];
+                }
+
+                curr[target] = take + skip;
+            }
+            prev = curr;
+        }
+
+        return prev[amount];
+
+    }
 }
