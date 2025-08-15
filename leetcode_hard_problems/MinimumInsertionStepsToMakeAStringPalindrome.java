8591d310327976ec1b162a90abd7a2f330c6b40f
Shivang <kshivang679@gmail.com>
2025-08-15 18:19:03 +0530

    added space optimized solution

--- leetcode_hard_problems/MinimumInsertionStepsToMakeAStringPalindrome.java
+++ leetcode_hard_problems/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -23,4 +23,29 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
         return n - dp[n][m];
 
     }
+
+    // space optimized approach
+    // this approach uses O(m) space
+    public int minInsertions2(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int m = reversed.length();
+
+        int[] prev = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == reversed.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                }
+            }
+
+            prev = curr;
+        }
+        return n - prev[m];
+
+    }
 }
