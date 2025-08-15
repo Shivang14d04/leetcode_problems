9ed0732f3a2c019ff01ac66139b2eae41a47c20f
Shivang <kshivang679@gmail.com>
2025-08-15 18:16:32 +0530

    solved using tabulation

---
+++ leetcode_hard_problems/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -0,0 +1,26 @@
+
+public class MinimumInsertionStepsToMakeAStringPalindrome {
+
+    // tabulation approach
+    // this approach uses O(n * m) space
+    // time complexity is O(n * m)
+    public int minInsertions(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int m = reversed.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == reversed.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return n - dp[n][m];
+
+    }
+}
