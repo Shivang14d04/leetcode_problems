609e0e49f73c20ce8f630c6b8e1fe9d8367fd312
Shivang <kshivang679@gmail.com>
2025-08-15 15:43:38 +0530

    added space optimised solution

--- leetcode_medium_problems/LongestPalindromeSubsequence.java
+++ leetcode_medium_problems/LongestPalindromeSubsequence.java
@@ -24,4 +24,30 @@ public class LongestPalindromeSubsequence {
         return dp[n][m];
 
     }
+
+    // space optimized approach
+    // this approach uses O(m) space
+    // where m is the length of the string's reverse
+    // time complexity is O(n * m)
+    public int longestPalindromeSubseq2(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        int n = s.length();
+        int m = reversed.length();
+        int[] prev = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == reversed.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = Math.max(curr[j - 1], prev[j]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+
+    }
 }
