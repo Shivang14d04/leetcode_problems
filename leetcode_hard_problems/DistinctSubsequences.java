5691fc7706d05c12dcca0ab2a5d721c21d7d6744
Shivang <kshivang679@gmail.com>
2025-08-16 20:08:56 +0530

    solved using tabulation and space optimization

--- leetcode_hard_problems/DistinctSubsequences.java
+++ leetcode_hard_problems/DistinctSubsequences.java
@@ -28,4 +28,43 @@ public class DistinctSubsequences {
 
         return dp[i][j] = take + notTake;
     }
+
+    // tabulation approach
+    // time complexity: O(n * m)
+    // space complexity: O(n * m)
+    public int numDistinct2(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    // space complexity: O(n * m)
+    // optimized space complexity: O(m)
+    public int numDistinct3(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[] dp = new int[m + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[m];
+    }
 }
