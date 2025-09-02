d5507cbb8830e48bf0783fc4feea696972bb9e80
Shivang <kshivang679@gmail.com>
2025-09-02 18:38:03 +0530

    solved formatting error

--- leetcode_hard_problems/MaximumRectangle.java
+++ leetcode_hard_problems/MaximumRectangle.java
@@ -9,7 +9,7 @@ public class MaximumRectangle {
 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(matrix[i][j] =='1'){
+                if(matrix[i][j] =='1'){ 
                     col[j]++;
                 }
                 else{
