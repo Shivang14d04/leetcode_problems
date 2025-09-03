dddcfa4e2d8427c1a151d264b43a5b0e53022d54
Shivang <kshivang679@gmail.com>
2025-09-03 07:22:45 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-solutions/problems/number_of_islands/solution.java
+++ my-solutions/problems/number_of_islands/solution.java
@@ -1,27 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
+        int n = grid.length;
+        int  m = grid[0].length;
+        int[][] visited = new int[n][m];
+        int count =0;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == '1' && visited[i][j] ==0){
 
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[i].length;j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    clearRestOfLand(grid , i ,j);
+                  dfs(visited,i,j,grid); 
+                  count++; 
                 }
             }
         }
         return count;
         
     }
-    private void clearRestOfLand(char[][] grid , int i , int j ){
-        if(i<0 || j<0 || i>= grid.length || j >= grid[i].length || grid[i][j] =='0'){
-            return;
+    public void dfs(int[][] visited , int row, int col , char [][] grid){
+        visited[row][col]  = 1;
+        int[] dx = {-1,0,1,0};
+        int[] dy  = {0,-1,0,1};
+        for(int i = 0;i<4;i++){
+            int nrow = row+ dx[i];
+            int ncol = col + dy[i];
+
+            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol< grid[0].length && visited[nrow][ncol] == 0 && grid[nrow][ncol] =='1'){
+                dfs(visited,nrow,ncol,grid);
+            }
         }
-        grid[i][j] = '0';
-        clearRestOfLand(grid , i+1 ,j);
-        clearRestOfLand(grid , i ,j+1);
-        clearRestOfLand(grid , i-1 ,j);
-        clearRestOfLand(grid , i ,j-1);
-        return;
     }
 }
