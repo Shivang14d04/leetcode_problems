e331685da43c9bdc551bf990eab671ac00410713
Shivang <kshivang679@gmail.com>
2025-09-28 11:39:42 +0530

    added optimised solution in O(m*n)

--- leetcode_medium_problems/SetMatrixZero.java
+++ leetcode_medium_problems/SetMatrixZero.java
@@ -1,4 +1,5 @@
 public class SetMatrixZero {
+    // time complexity: O(m*n*(m+n))
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -30,4 +31,35 @@ public class SetMatrixZero {
             }
         }
     }
+    // time complexity: O(m*n)
+     public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int [] col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                     row[i] = 1;
+                     col[j] = 1;
+
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if(row[i]==1){
+                for(int j  =0;j<n;j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+         for (int i = 0; i < n; i++) {
+            if(col[i]==1){
+                for(int j  =0;j<m;j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+    }
+
 }
