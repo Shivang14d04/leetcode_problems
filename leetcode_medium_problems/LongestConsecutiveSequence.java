b0bef3f32500906df466ace8b3ec29552355a228
Shivang <kshivang679@gmail.com>
2025-09-29 11:16:04 +0530

    solved in O(n)

--- leetcode_medium_problems/LongestConsecutiveSequence.java
+++ leetcode_medium_problems/LongestConsecutiveSequence.java
@@ -22,4 +22,31 @@ public class LongestConsecutiveSequence{
         return max +1;
         
     }
+    // time complexity O(n)
+    public int longestConsecutive2(int[] nums) {
+
+        if (nums.length == 1)
+            return 1;
+        if (nums.length == 0)
+            return 0;
+        int max = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int it : set) {
+            if (!set.contains(it - 1)) {
+                int x = it;
+                int length = 1;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    length++;
+                }
+                max = Math.max(length, max);
+            }
+        }
+
+        return max;
+
+    }
 }
