e66e037a50172c433c1c52f9baf35204752aaf01
Shivang <kshivang679@gmail.com>
2025-10-12 20:36:45 +0000

    Sync LeetCode submission - Next Greater Element II (java)

---
+++ my-solutions/problems/next_greater_element_ii/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        for (int i = 0; i < n; i++) {
+            int currEle = arr[i];
+
+            for (int j = 1; j < n; j++) {
+                int ind = (i + j) % n;
+
+                if (arr[ind] > currEle) {
+                    ans[i] = arr[ind];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    
+    }
+}
