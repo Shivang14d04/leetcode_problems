ca99c22bc8f2362040f6cd2e78829060febbd3c4
Shivang <kshivang679@gmail.com>
2025-12-09 17:24:40 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (java)

--- my-solutions/problems/search_in_rotated_sorted_array_ii/solution.java
+++ my-solutions/problems/search_in_rotated_sorted_array_ii/solution.java
@@ -1,32 +1,34 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                return true;
-            } 
-            if(arr[start] == arr[mid] &&arr[mid] == arr[end]){
-                start++;
-                end--;
-            
+    public boolean search(int[] nums, int target) {
+       int start = 0;
+       int n = nums.length;
+       int end = n-1;
+       while(start<=end){
+        int mid = start +(end-start)/2;
+        if(nums[mid]==target){
+            return true;
+        }
+        if(nums[mid]==nums[start] && nums[mid]== nums[end]){
+            start++;
+            end--;
+        }
+        else if(nums[mid]>=nums[start]){
+            if(nums[mid]>=target && nums[start]<=target){
+                end = mid-1;
             }
-            else if (arr[mid] >= arr[start]) {
-                if (target <= arr[mid] && target >= arr[start]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-
-            } else {
-                if (target >= arr[mid] && target <= arr[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+            else{
+                start = mid+1;
             }
         }
-        return false;
+        else{
+            if(target>=nums[mid] && target<=nums[end]){
+                start = mid +1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+       }
+       return false; 
     }
 }
