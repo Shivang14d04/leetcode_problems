2ea979163ab48e4f40d6ff576f74249f12fd60af
Shivang <kshivang679@gmail.com>
2025-12-20 19:24:05 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

--- my-solutions/problems/median_of_two_sorted_arrays/solution.java
+++ my-solutions/problems/median_of_two_sorted_arrays/solution.java
@@ -1,44 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length + nums2.length;
-       int[] arr = new int[n];
-       int s = 0;
-       int e = 0;
-       int i = 0;
-       while(s<nums1.length && e<nums2.length){
-        if(nums1[s]>=nums2[e]){
-            arr[i] = nums2[e];
-            e++;
-            i++;
-        }
-        else{
-            arr[i] = nums1[s];
-            s++;
-            i++;
-        }
-       }
-       if(s==nums1.length){
-        while(e<nums2.length){
-            arr[i]=nums2[e];
-            e++;
-            i++;
-        }
-       }
-        if(e==nums2.length){
-        while(s<nums1.length){
-            arr[i]=nums1[s];
-            s++;
-            i++;
-        }
-       }
-        double ans = 0;
-       if(n%2==0){
-        ans = (arr[(n/2) - 1] + arr[n/2])/2.0;
-       }
-       else{
-        ans = arr[n/2];
-       }
+        int a = nums1.length;
+        int b = nums2.length;
+        if (a > b)
+          return   findMedianSortedArrays(nums2, nums1);
+        int start = 0;
+        int end = a;
+        while (start <= end) {
+            int mid1 = start + ((end - start) >> 1);
+            int mid2 = ((a + b + 1) >> 1) - mid1;
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int r1 = (mid1 == a) ? Integer.MAX_VALUE : nums1[mid1];
 
-        return ans;
+            int r2 = (mid2 == b) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if(l1<=r2 && l2<=r1){
+                if((a+b)%2==0){
+                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+                }
+                else{
+                    return Math.max(l1,l2);
+                }
+
+            }
+            else if(l1>r2){
+                end = mid1 -1;
+            }
+            else{
+                start = mid1+1;
+            }
+
+        }
+        return 0.0;
     }
 }
