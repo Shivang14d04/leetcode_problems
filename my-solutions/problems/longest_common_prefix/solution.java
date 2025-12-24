d9eaf0bfbc3cba419c0d3efb3ab0bc25b8dcf68e
Shivang <kshivang679@gmail.com>
2025-12-24 04:42:48 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- my-solutions/problems/longest_common_prefix/solution.java
+++ my-solutions/problems/longest_common_prefix/solution.java
@@ -1,18 +1,15 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder string = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
-
-        for(int i = 0;i<Math.min(first.length() , last.length());i++){
-            if(first.charAt(i)!=last.charAt(i)){
-                return string.toString();
+        String first   = strs[0];
+        String last = strs[strs.length -1];
+        for(int i = 0;i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i) != last.charAt(i)){
+                return result.toString();
             }
-            string.append(first.charAt(i));
-
+            result.append(first.charAt(i));
         }
-        return string.toString();
-        
+        return result.toString();
     }
 }
