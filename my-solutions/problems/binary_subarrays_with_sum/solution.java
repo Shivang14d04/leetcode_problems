46b5ca31f3e0f32b856848129296bc8767f4a561
Shivang <kshivang679@gmail.com>
2026-01-02 06:50:00 +0000

    Sync LeetCode submission - Binary Subarrays With Sum (java)

--- my-solutions/problems/binary_subarrays_with_sum/solution.java
+++ my-solutions/problems/binary_subarrays_with_sum/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+       return SumLesserThanAndEqual(nums,goal)- SumLesserThanAndEqual(nums,goal-1);
+
+        
+    }
+    public int SumLesserThanAndEqual(int[]nums,int goal){
+        if(goal<0) return 0;
         int n = nums.length;
         int count = 0;
         int sum = 0;
-        for(int i=0;i<n;i++){
-            sum =0;
-            for(int j = i;j<n;j++){
-                sum += nums[j];
-                if(sum == goal){
-                    count++;
-                }
-
+        int left = 0;
+        int right = 0;
+        while(right<n){
+            sum+=nums[right];
+            while(sum>goal){
+                sum -= nums[left];
+                left = left +1;
             }
+            count = count + (right-left+1);
+            right++;
         }
         return count;
     }
