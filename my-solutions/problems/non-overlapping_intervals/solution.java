7ab5f41c6df0200525dfb8cb5937dfa585fbe5ca
Shivang <kshivang679@gmail.com>
2026-01-12 07:50:15 +0000

    Sync LeetCode submission - Non-overlapping Intervals (java)

---
+++ my-solutions/problems/non-overlapping_intervals/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> a[1] - b[1]);
+        int count = 1;
+        int n = intervals.length;
+        int prevInterval = intervals[0][1];
+        for(int i = 1;i< n;i++){
+            if(intervals[i][0] >= prevInterval){
+                count++;
+                prevInterval = intervals[i][1];
+            }
+        }
+        return n - count;
+
+        
+    }
+}
