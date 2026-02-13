5b4a7d272a848271d382ed581f7286428637b6d2
Shivang <kshivang679@gmail.com>
2026-02-13 14:45:40 +0000

    Sync LeetCode submission - Binary Search (java)

--- my-solutions/problems/binary_search/solution.java
+++ my-solutions/problems/binary_search/solution.java
@@ -1,24 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-
-        return binarySearch(start , end  , target , nums);
-
-        
-    }
-    public static int binarySearch(int start , int end , int target , int [] nums){
-        if(start>end) return -1;
-        int mid = start +(end-start)/2;
-
-        if(nums[mid]== target) return mid;
-        else if(nums[mid]>target){
-           return binarySearch(start , mid -1 , target , nums);
-        }
-        else{
-            return binarySearch(mid+1 ,  end , target , nums);
-
-        }
-        
+        int index = Arrays.binarySearch(nums,target);
+        return index >= 0 ? index : -1;
     }
 }
