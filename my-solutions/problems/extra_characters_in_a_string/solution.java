36caac37851b3c5a5976fa1f684785bb37701f0c
Shivang <kshivang679@gmail.com>
2026-02-19 05:20:37 +0000

    Sync LeetCode submission - Extra Characters in a String (java)

---
+++ my-solutions/problems/extra_characters_in_a_string/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        Set<String> set = new HashSet<>(Arrays.asList(dictionary));
+        int n = s.length();
+        int [] dp = new int[n+1];
+        for(int i = n-1;i>=0;i--){
+            dp[i] = 1+dp[i+1];
+            for(int j = i;j<n;j++){
+            if(set.contains(s.substring(i,j+1))){
+                dp[i] = Math.min(dp[i], dp[j+1]);
+            }                
+            }
+        }
+        return dp[0];
+    }
+}
