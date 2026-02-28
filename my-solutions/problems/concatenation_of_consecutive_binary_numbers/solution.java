a4903ca24d1064d9239ba1669ef7e81517b1b535
Shivang <kshivang679@gmail.com>
2026-02-28 18:28:36 +0000

    Sync LeetCode submission - Concatenation of Consecutive Binary Numbers (java)

---
+++ my-solutions/problems/concatenation_of_consecutive_binary_numbers/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        int length = 0;
+        int mod = 1000000007;
+        long result = 0;
+        for(int i =1;i<=n;i++){
+            if((i & (i-1))==0){
+                length++;
+           }
+           result = ((result<<length)+i)%mod;
+        }
+        return (int)result;
+
+    }
+}
