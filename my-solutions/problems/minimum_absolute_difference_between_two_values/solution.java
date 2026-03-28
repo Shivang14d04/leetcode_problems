10179c12f5d244ea60fd406488c33b34a6644174
Shivang <kshivang679@gmail.com>
2026-03-28 14:42:13 +0000

    Sync LeetCode submission - Minimum Absolute Difference Between Two Values (java)

---
+++ my-solutions/problems/minimum_absolute_difference_between_two_values/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minAbsoluteDifference(int[] nums) {
+        int last1 = -1, last2 = -1;
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+                last1 = i;
+                if (last2 != -1) {
+                    ans = Math.min(ans, Math.abs(i - last2));
+                }
+            }
+
+            if (nums[i] == 2) {
+                last2 = i;
+                if (last1 != -1) {
+                    ans = Math.min(ans, Math.abs(i - last1));
+                }
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
