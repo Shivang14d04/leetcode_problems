338aa80ba721e4c34a418ee7197451c1d52d8842
Shivang <kshivang679@gmail.com>
2026-04-02 06:52:05 +0000

    Sync LeetCode submission - Average Time of Process per Machine (mysql)

---
+++ my-solutions/problems/average_time_of_process_per_machine/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select a.machine_id , round(avg(b.timestamp - a.timestamp),3) as processing_time from Activity a
+join Activity b on a.machine_id = b.machine_id AND a.process_id = b.process_id AND 
+a.activity_type = "start" AND b.activity_type = "end"
+group by a.machine_id;
