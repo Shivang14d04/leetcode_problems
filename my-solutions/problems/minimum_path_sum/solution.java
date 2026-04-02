8ad84fff42e6cf4c4a10d1b0208d952c269bb779
Shivang <kshivang679@gmail.com>
2026-04-02 05:44:13 +0000

    Sync LeetCode submission - Minimum Path Sum (java)

--- my-solutions/problems/minimum_path_sum/solution.java
+++ my-solutions/problems/minimum_path_sum/solution.java
@@ -3,34 +3,24 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
 
-        for (int[] a : dp) {
-            Arrays.fill(a, -1);
-        }
+                if(j==0 && i==0)dp[i][j] = grid[i][j];
+                else{
+                int up = grid[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += (int)1e9;
 
-        return helper(grid, dp, m - 1, n - 1);
+                int left = grid[i][j];
+                if(j>0) left += dp[i][j-1];
+                else left += (int)1e9;
 
-    }
+                dp[i][j] = Math.min(up,left);
+                }
 
-    public int helper(int[][] grid, int[][] dp, int i, int j) {
-        if (i == 0 && j == 0) {
-            return grid[0][0];
+            }
         }
-        if (i < 0 || j < 0) {
-            return Integer.MAX_VALUE;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int up = helper(grid, dp, i - 1, j);
-        int left = helper(grid, dp, i, j - 1);
-        int minPath = Math.min(up, left);
-        if (minPath == Integer.MAX_VALUE) {
-            dp[i][j] = Integer.MAX_VALUE;
-        } else {
-            dp[i][j] = grid[i][j] + minPath;
-        }
-        return dp[i][j];
+        return dp[m-1][n-1];
     }
 }
