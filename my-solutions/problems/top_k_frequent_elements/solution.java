75b9f1af348bb88a3dd4c8f67cdd7aecaa7d980f
Shivang <kshivang679@gmail.com>
2026-04-04 17:58:34 +0000

    Sync LeetCode submission - Top K Frequent Elements (java)

--- my-solutions/problems/top_k_frequent_elements/solution.java
+++ my-solutions/problems/top_k_frequent_elements/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       HashMap<Integer,Integer> map = new HashMap<>();
-       int n = nums.length;
-        for(int i = 0;i< n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+      Map<Integer,Integer> map = new HashMap<>();
+      for(int num : nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+      }  
+      PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
+      for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+        pq.offer(entry.getKey());
+        if(pq.size()>k){
+            pq.poll();
         }
-        PriorityQueue<int[] > pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            pq.offer(new int[]{entry.getValue(), entry.getKey()});
-            while(pq.size()>k){
-                pq.poll();
-            }
-        }
-
-        int [] ans = new int[k];
-        for(int i =0;i<k;i++){
-            ans[i] = pq.poll()[1];
-        }
-        return ans;
+      }
+      int [] ans = new int[k];
+      for(int i =0;i<k;i++){
+        ans[i]= pq.poll();
+      }
+      return ans;
     }
 }
