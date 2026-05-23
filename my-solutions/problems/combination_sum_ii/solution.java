0f8ff1667c02e58a19a1cb8416fbf7166f51015a
Shivang <kshivang679@gmail.com>
2026-05-23 05:09:14 +0000

    Sync LeetCode submission - Combination Sum II (java)

--- my-solutions/problems/combination_sum_ii/solution.java
+++ my-solutions/problems/combination_sum_ii/solution.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result  = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
         Arrays.sort(candidates);
-        helper(result, comb,candidates,target, 0);
-        return result;
+        List<Integer> list  = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(0,list,ans,candidates,target);
+        return ans;
+
     }
-    public void helper(List<List<Integer>> result,List<Integer> comb, int [] nums, int target, int index ){
-        if(target== 0){
-             result.add(new ArrayList<>(comb));
+    public void helper(int index, List<Integer> list, List<List<Integer>> ans, int[] candidates , int target){
+
+        if(target == 0){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if(target < 0 || index > nums.length-1){
-            return;
+        for(int i = index;i<candidates.length;i++){
+            if(i> index && candidates[i]== candidates[i-1])continue;
+            if(candidates[i]>target)break;
+            list.add(candidates[i]);
+            helper(i+1,list,ans,candidates,target-candidates[i]);
+            list.remove(list.size()-1);
         }
-
-        comb.add(nums[index]);
-
-        helper(result,comb,nums,target-nums[index], index+1);
-        comb.remove(comb.size()-1);
-                while(index+1 < nums.length && nums[index+1] == nums[index]){
-            index++;
-        }
-        helper(result,comb,nums,target,index+1);
     }
+
 }
