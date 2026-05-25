064524f8af660a790f8c3aa77720e5007f3da638
Shivang <kshivang679@gmail.com>
2026-05-25 20:01:23 +0000

    Sync LeetCode submission - N-Queens (java)

--- my-solutions/problems/n-queens/solution.java
+++ my-solutions/problems/n-queens/solution.java
@@ -1,55 +1,39 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        char [][] board = new char[n][n];
-        for(int i = 0;i<n;i++){
-            for(int j =0;j<n;j++){
-                board[i][j] = '.';
-            }
+        List<List<String>> ans  = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
         }
-
-        helper(0 ,result , n , board);
-        return result;
-        
+        int[] leftRow = new int[n];
+        int[] upperDiagonal = new int[2*n-1];
+        int [] lowerDiagonal = new int[2*n-1];
+        helper(0,ans,board,n,leftRow,upperDiagonal,lowerDiagonal);
+        return ans;
     }
-    public void helper(int row, List<List<String>> result,int n, char[][] board){
-        if(row == n){
-            result.add(constructBoard(board));
+    public void helper(int col,List<List<String>> ans, char[][]board, int n,int[] leftRow, int[] upperDiagonal,int []lowerDiagonal ){
+        if(col == n){
+            List<String> temp = new ArrayList<>();
+            for(int i =0;i<n;i++){
+                temp.add(new String(board[i]));
+            }
+            ans.add(temp);
             return;
         }
 
-        for(int col = 0;col<n; col++){
-            if(Safe(row , col , board ,n)){
-                board[row][col] ='Q';
-                helper(row+1 , result , n,board);
-                board[row][col] = '.';
+        for(int row =0;row<n;row++){
+            if(leftRow[row]==0 && lowerDiagonal[col+row]==0 && upperDiagonal[n-1+col-row]==0){
+                board[row][col]='Q';
+                leftRow[row]=1;
+                lowerDiagonal[col+row]=1 ;
+                upperDiagonal[n-1+col-row]=1;
+                helper(col+1,ans,board,n,leftRow,upperDiagonal,lowerDiagonal);
+                board[row][col] ='.';
+                leftRow[row]=0;
+                lowerDiagonal[col+row]=0 ;
+                upperDiagonal[n-1+col-row]=0;                
             }
         }
-
-    }
-
-    public boolean Safe(int row , int col ,char[][] board,int n){
-        for(int i = 0;i<row;i++){
-            if(board[i][col] =='Q') return false;
-        }
-
-        for(int i = row-1 , j= col-1 ; i>=0 && j>=0 ;i--,j--){
-            if(board[i][j] == 'Q') return false;
-        }
-                for(int i = row-1 , j= col+1 ; i>=0 && j<n ;i--,j++){
-            if(board[i][j] == 'Q') return false;
-        }
-        return true;
-
-    }
-
-    public List<String> constructBoard(char[][]board){
-        List<String> current = new ArrayList<>();
-
-        for(int i = 0;i<board.length;i++){
-            current.add(new String(board[i]));
-        }
-        return current;
     }
 
 }
