e0046821f84831c73c107c40ff97b11eb1b4566d
Shivang <kshivang679@gmail.com>
2026-06-03 12:36:01 +0000

    Sync LeetCode submission - Sudoku Solver (java)

--- my-solutions/problems/sudoku_solver/solution.java
+++ my-solutions/problems/sudoku_solver/solution.java
@@ -1,41 +1,48 @@
 class Solution {
     public void solveSudoku(char[][] board) {
         helper(board);
-        
     }
 
-    public boolean helper(char[][] board){
-        for(int row = 0;row< 9;row++){
-            for(int col = 0;col<9;col++){
-                if(board[row][col]=='.'){
-                    for(char num = '1';num<='9';num++){
-                        if(isValid(board , row , col , num)){
-                            board[row][col] = num;
-                            if(helper(board)){
+    public boolean helper(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(i, j, board, ch)) {
+                            board[i][j] = ch;
+                            if (helper(board))
                                 return true;
-                            }
-
-                            board[row][col] ='.';
+                            board[i][j] = '.';
                         }
                     }
-                    return false;
+                                    return false;
+
                 }
+            }
+
+        }
+        return true;
+    }
+
+    public boolean isValid(int row, int col, char[][] board, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == ch)
+                return false;
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch)
+                return false;
+        }
+
+        int boxrowStart = 3 * (row / 3);
+        int boxColStart = 3 * (col / 3);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[boxrowStart + i][boxColStart + j] == ch)
+                    return false;
             }
         }
         return true;
     }
-
-    public boolean isValid(char[][]board , int row , int col , char num){
-        for(int i = 0;i<9;i++){
-            if(board[i][col] == num) return false;
-            if(board[row][i] == num) return false;
-
-            int boxRow = 3 * (row/3) + i/3;
-            int boxCol = 3 * (col/3) + i%3;
-            if(board[boxRow][boxCol] == num) return false;
-
-        }
-        return true;
-    }
-    
 }
