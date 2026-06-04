7cba2a48a8afadc9659326ec8d4639742cb3570c
Shivang <kshivang679@gmail.com>
2026-06-04 08:40:25 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-solutions/problems/number_of_islands/solution.java
+++ my-solutions/problems/number_of_islands/solution.java
@@ -1,32 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
-        int  m = grid[0].length;
-        int[][] visited = new int[n][m];
-        int count =0;
-        
-        for(int i = 0;i<n;i++){
+        int m = grid[0].length;
+        int count = 0;
+        int[][] vis = new int[n][m];
+        for(int i =0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j] == '1' && visited[i][j] ==0){
-
-                  dfs(visited,i,j,grid); 
-                  count++; 
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    dfs(i,j,vis,grid);
+                    count++;
                 }
             }
         }
         return count;
-        
     }
-    public void dfs(int[][] visited , int row, int col , char [][] grid){
-        visited[row][col]  = 1;
-        int[] dx = {-1,0,1,0};
-        int[] dy  = {0,-1,0,1};
-        for(int i = 0;i<4;i++){
-            int nrow = row+ dx[i];
-            int ncol = col + dy[i];
 
-            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol< grid[0].length && visited[nrow][ncol] == 0 && grid[nrow][ncol] =='1'){
-                dfs(visited,nrow,ncol,grid);
+    public void dfs(int row, int col, int [][] vis , char[][] grid){
+        vis[row][col] = 1;
+        int[] dx = {-1,0,1,0};
+        int [] dy = {0,-1,0,1};
+        for(int i =0;i<4;i++){
+            int r = row+ dx[i];
+           int  c = col + dy[i];
+            if(r>=0 && c>=0 && r< grid.length && c< grid[0].length && vis[r][c]==0 && grid[r][c]=='1'){
+                 dfs(r,c,vis,grid);
             }
         }
     }
