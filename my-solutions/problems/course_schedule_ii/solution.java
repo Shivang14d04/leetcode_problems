ce41d11ed06fa9f47464bfed2f0a2288ca95d0fc
Shivang <kshivang679@gmail.com>
2026-06-06 12:32:45 +0000

    Sync LeetCode submission - Course Schedule II (java)

--- my-solutions/problems/course_schedule_ii/solution.java
+++ my-solutions/problems/course_schedule_ii/solution.java
@@ -1,25 +1,19 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int V = numCourses;
-        for (int i = 0; i < V; i++) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < prerequisites.length; i++) {
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            adj.get(b).add(a);
+        int[] indegree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            indegree[pre[0]]++;
+            adj.get(pre[1]).add(pre[0]);
         }
-        int[] inDegree = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int it : adj.get(i)) {
-                inDegree[it]++;
-            }
-        }
+
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (inDegree[i] == 0) {
-                q.add(i);
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
             }
         }
         ArrayList<Integer> ans = new ArrayList<>();
@@ -27,18 +21,21 @@ class Solution {
             int node = q.poll();
             ans.add(node);
             for (int it : adj.get(node)) {
-                inDegree[it]--;
-                if (inDegree[it] == 0) {
-                    q.add(it);
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.offer(it);
                 }
             }
         }
 
-        if (ans.size() == V) {
-            int[] res = new int[V];
-            for (int i = 0; i < V; i++) res[i] = ans.get(i);
-            return res;
+        if (ans.size() != numCourses) {
+            return new int[0];
         }
-        return new int[0]; 
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+
     }
 }
