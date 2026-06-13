48cf8c088118895e07903bd6ada0bde8031acb1d
Shivang <kshivang679@gmail.com>
2026-06-13 07:12:08 +0000

    Sync LeetCode submission - Cheapest Flights Within K Stops (java)

--- my-solutions/problems/cheapest_flights_within_k_stops/solution.java
+++ my-solutions/problems/cheapest_flights_within_k_stops/solution.java
@@ -1,42 +1,36 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for(int i = 0;i<n;i++){
+        ArrayList<ArrayList<int []>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i = 0;i<flights.length;i++){
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int weight = flights[i][2];
-            adj.get(u).add(new int[]{v,weight});
+        for(int [] a: flights){
+            int u = a[0];
+            int v = a[1];
+            int wt = a[2];
+            adj.get(u).add(new int[]{v,wt});
         }
-
-        int[] dis = new int[n];
-        Arrays.fill(dis,(int)1e9);
-        dis[src] =0;
+        int [] dis = new int[n];
+        Arrays.fill(dis, (int)1e9);
+        dis[src]=0;
         Queue<int[]> pq = new LinkedList<>();
-        pq.add(new int[]{0,src,0});
+        pq.offer(new int[]{src,0,0});
+
         while(!pq.isEmpty()){
-            int [] arr = pq.poll();
-            int distance = arr[2];
-            int node = arr[1];
-            int stops = arr[0];
-           
-            if(stops>k) continue;
+            int node = pq.peek()[0];
+            int d = pq.peek()[1];
+            int stops = pq.peek()[2];
+            pq.remove();
+            if(stops > k ) continue;
             for(int[] it: adj.get(node)){
-                int edgeWeight = it[1];
-                int nextNode = it[0];
-            
-            if(dis[nextNode] > distance + edgeWeight &&  stops<=k ){
-                dis[nextNode] = distance+edgeWeight;
-                pq.add(new int[]{stops+1,nextNode, dis[nextNode]});
+                int next = it[0];
+                int w = it[1];
+                if(d + w < dis[next] && stops<=k){
+                    dis[next] = d + w;
+                    pq.offer(new int[]{next,dis[next], stops+1});
+                }
             }
-            }
-
-
         }
-        if(dis[dst]== (int)1e9) return -1;
-        return dis[dst];
-
+        return dis[dst]== (int)1e9 ? -1:dis[dst];
     }
 }
