54f064343e933a9c75b4b29b7ea60db1ddfc75ae
Shivang <kshivang679@gmail.com>
2026-06-13 16:24:06 +0000

    Sync LeetCode submission - Swim in Rising Water (java)

--- my-solutions/problems/swim_in_rising_water/solution.java
+++ my-solutions/problems/swim_in_rising_water/solution.java
@@ -1,28 +1,28 @@
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        boolean[][] visited = new boolean[n][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[0]-b[0]);
-        pq.offer(new int[]{grid[0][0], 0 ,0});
-        visited[0][0] = true;
+        boolean [][] vis = new boolean[n][n];
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.offer(new int[]{grid[0][0],0,0});
+
+
         while(!pq.isEmpty()){
-            int [] arr = pq.poll();
-            int elevation = arr[0];
-            int row = arr[1];
-            int col = arr[2];
-            int[] dx  = {-1,0,1,0};
+            int [] temp  = pq.poll();
+            int h = temp[0];
+            int row = temp[1];
+            int col = temp[2];
+            if(row == n-1 && col == n-1) return h;
+            vis[row][col] = true;
+            int [] dx = {-1,0,1,0};
             int [] dy = {0,-1,0,1};
-            if(row==n-1 && col == n-1) return elevation;
-            for(int i = 0;i<4;i++){
-                int newR = row + dx[i];
-                int newC = col + dy[i];
-                if(newR>=0 && newC>=0 && newR< n && newC < n && !visited[newR][newC]){
-                    visited[newR][newC] =true;
-                    pq.offer(new int[]{Math.max(elevation, grid[newR][newC]) , newR, newC});
+            for(int i =0;i<4;i++){
+                int r = row + dx[i];
+                int c = col + dy[i];
+                if(r>=0 && c >=0 && r< n&& c<n && !vis[r][c] ){
+                    pq.offer(new int[]{Math.max(h,grid[r][c]),r,c});
                 }
             }
         }
         return -1;
-        
     }
 }
