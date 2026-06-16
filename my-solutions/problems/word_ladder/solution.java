bc5ede71f6fada4ba728fbe298afe9733a780b41
Shivang <kshivang679@gmail.com>
2026-06-16 06:50:54 +0000

    Sync LeetCode submission - Word Ladder (java)

--- my-solutions/problems/word_ladder/solution.java
+++ my-solutions/problems/word_ladder/solution.java
@@ -1,50 +1,38 @@
-public class Pair{
+class Pair{
     String first;
     int second;
-    Pair(String first,int second){
-        this.first = first;
-        this.second = second;
+    Pair(String _first , int _second){
+        this.first  = _first;
+        this.second = _second;
     }
 }
 
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>();
         Queue<Pair> q = new LinkedList<>();
         q.offer(new Pair(beginWord,1));
-        Set<String> set = new HashSet<>();
-        int n = wordList.size();
-        for(int i = 0;i<n;i++){
+        for(int i =0;i<wordList.size();i++){
             set.add(wordList.get(i));
         }
-
         set.remove(beginWord);
-
         while(!q.isEmpty()){
-
             String word = q.peek().first;
-            int steps  = q.peek().second;
+            int length = q.peek().second;
             q.remove();
-
-
-            if(word.equals(endWord)){
-                return steps;
-            }
-
-            for(int i = 0;i<beginWord.length();i++){
-                for(char ch = 'a' ; ch<='z';ch++){
-                    char [] replacedword = word.toCharArray();
-                    replacedword[i] = ch;
-                    String replacedWord = new String(replacedword);
-
+            if(word.equals(endWord)) return length;
+            for(int i=0;i<word.length();i++){
+                for(char ch = 'a';ch<='z';ch++){
+                    char [] replacedCharArray = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedWord = new String(replacedCharArray);
                     if(set.contains(replacedWord)){
+                        q.offer(new Pair(replacedWord,length+1));
                         set.remove(replacedWord);
-                        q.offer(new Pair(replacedWord, steps+1));
                     }
                 }
             }
-
         }
         return 0;
-        
     }
 }
