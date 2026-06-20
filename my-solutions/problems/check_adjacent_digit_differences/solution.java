51cdc13468b92260fa716cedc74d218b9b55652e
Shivang <kshivang679@gmail.com>
2026-06-20 05:27:39 +0000

    Sync LeetCode submission - Check Adjacent Digit Differences (java)

---
+++ my-solutions/problems/check_adjacent_digit_differences/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        int n = s.length();
+        for(int i=0;i<n-1;i++){
+                int a   = s.charAt(i) -'0';
+                int b = s.charAt(i+1)-'0';
+                if(Math.abs(a-b) > 2) return false;
+            
+        }
+        return true;
+    }
+}
