4c362a2b7be14b90d96f1ac6520a9db594245749
Shivang <kshivang679@gmail.com>
2026-06-21 08:11:16 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- my-solutions/problems/symmetric_tree/solution.java
+++ my-solutions/problems/symmetric_tree/solution.java
@@ -16,13 +16,13 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null) return true;
-       return helper(root.left, root.right);
-
+       return helper(root.left,root.right);
     }
-    public boolean  helper(TreeNode n1, TreeNode n2){
-        if(n1== null && n2==null) return true;
-        if(n1 == null || n2 == null) return false;
-        if(n1.val != n2.val) return false;
-        return helper(n1.left, n2.right) && helper(n1.right , n2.left);
+    public boolean helper(TreeNode node1, TreeNode node2){
+        if(node1==null && node2==null)return true;
+
+        if(node1==null ||node2==null)return false;
+        if(node1.val != node2.val)return false;
+        return helper(node1.left,node2.right) && helper(node1.right, node2.left);
     }
 }
