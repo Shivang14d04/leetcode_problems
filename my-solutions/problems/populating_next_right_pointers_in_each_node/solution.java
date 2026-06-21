153081d09197b61062378707f8d1da338eb34b16
Shivang <kshivang679@gmail.com>
2026-06-21 16:14:14 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (java)

--- my-solutions/problems/populating_next_right_pointers_in_each_node/solution.java
+++ my-solutions/problems/populating_next_right_pointers_in_each_node/solution.java
@@ -23,19 +23,27 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return null;
-        
-        Node LeftMost = root;
-        while(LeftMost.left != null){
-            Node current = LeftMost;
-            while(current !=null){
-                current.left.next = current.right;
-                if(current.next != null){
-                    current.right.next = current.next.left;
+        if(root==null) return root;
+        Queue<Node> q= new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size= q.size();
+            for(int i =0;i<size;i++){
+                Node node  = q.poll();
+                if(i<size -1){
+                    node.next = q.peek();
                 }
-                current = current.next;
+                else{
+                    node.next = null;
+                }
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+
+                if(node.right != null){
+                    q.offer(node.right);
+                }
             }
-            LeftMost = LeftMost.left;
         }
         return root;
     }
