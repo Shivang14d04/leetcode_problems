b82f14f38b00c2f8f8f51cd7ffbe44958f75d772
Shivang <kshivang679@gmail.com>
2026-06-21 12:20:24 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (java)

--- my-solutions/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.java
+++ my-solutions/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.java
@@ -15,24 +15,20 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for(int i=0;i<inorder.length;i++){
+        map.put(inorder[i],i);
+       } 
+       return helper(preorder, 0, preorder.length -1, inorder , 0 , inorder.length-1,map);
     }
-
-    public TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, HashMap<Integer, Integer> map) {
-        if(preStart > preEnd || inStart > inEnd){
-            return null;
-        }
-
+    public  TreeNode helper(int[] preorder, int preStart, int preEnd , int[] inorder, int inStart, int inEnd, HashMap<Integer,Integer> map){
+        if(preStart > preEnd || inStart > inEnd)return null;
         TreeNode root = new TreeNode(preorder[preStart]);
         int rootIndex = map.get(root.val);
-        int leftElements  = rootIndex - inStart;
+        int leftPart = rootIndex - inStart ;
+        root.left = helper(preorder,preStart+ 1, preStart+leftPart, inorder, inStart, rootIndex -1, map);
+        root.right  = helper(preorder,preStart+leftPart +1, preEnd, inorder, rootIndex +1 , inEnd, map);        
 
-        root.left = build(preorder, preStart+1, preStart + leftElements, inorder,inStart, rootIndex -1, map);
-        root.right  = build(preorder, preStart + leftElements+1 , preEnd, inorder, rootIndex+1, inEnd,map);
         return root;
     }
 }
