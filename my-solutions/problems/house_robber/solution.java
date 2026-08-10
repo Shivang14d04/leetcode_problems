07a07e151427cdcadae59ca7efa10f38fd6c9f5d
Shivang <kshivang679@gmail.com>
2026-08-10 04:49:48 +0000

    Sync LeetCode submission - House Robber (java)

--- my-solutions/problems/house_robber/solution.java
+++ my-solutions/problems/house_robber/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int [] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return Math.max(helper(dp,0, nums), helper(dp,1, nums));
+        int prev1= nums[0], prev2 = 0;
+        int n  = nums.length;
+        for(int i=1;i<n;i++){
+            int curr = Math.max((nums[i]+ prev2), prev1);
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return prev1;
+        
+
     }
-    public int helper(int [] dp , int i , int [] nums){
-        if(i >= nums.length) return 0;
-        if(dp[i]!= -1)return dp[i];
-        dp[i] = nums[i] + Math.max(helper(dp, i+2, nums), helper(dp,i+3,nums));
-        return dp[i];
-    }
+
 }
