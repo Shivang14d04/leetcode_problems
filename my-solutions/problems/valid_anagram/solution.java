0d1b9b88da20bcc295abad2df085030df33a1ab7
Shivang <kshivang679@gmail.com>
2026-08-10 10:06:34 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- my-solutions/problems/valid_anagram/solution.java
+++ my-solutions/problems/valid_anagram/solution.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        if(n!=m) return false;
         int [] freq = new int[26];
-        for(int i =0;i< n;i++){
+        for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
-            freq[ch - 'a']++;
+            freq[ch-97]++; 
         }
-
-        for(int i =0;i<m;i++){
+        for(int i = 0;i<t.length();i++){
             char ch = t.charAt(i);
-            freq[ch-'a']--;
+            freq[ch-97]--;
         }
-        for(int i =0;i< freq.length;i++){
-            if(freq[i]>0) return false;
+        for(int i =0;i<26;i++){
+            if(freq[i]!=0)return false;
         }
         return true;
     }
