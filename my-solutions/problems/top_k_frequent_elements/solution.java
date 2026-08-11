ae7088c70f790cfc7506011d2479b8c59347d285
Shivang <kshivang679@gmail.com>
2026-08-11 07:40:28 +0000

    Sync LeetCode submission - Top K Frequent Elements (java)

--- my-solutions/problems/top_k_frequent_elements/solution.java
+++ my-solutions/problems/top_k_frequent_elements/solution.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-      Map<Integer,Integer> map = new HashMap<>();
-      for(int num : nums){
-        map.put(num,map.getOrDefault(num,0)+1);
-      }  
-      PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
-      for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-        pq.offer(entry.getKey());
-        if(pq.size()>k){
-            pq.poll();
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n,map.getOrDefault(n,0)+1);
         }
-      }
-      int [] ans = new int[k];
-      for(int i =0;i<k;i++){
-        ans[i]= pq.poll();
-      }
-      return ans;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            pq.add(new int[]{entry.getValue(),entry.getKey()});
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int [] ans = new int[k];
+        int i =0;
+        while(!pq.isEmpty()){
+            ans[i++] = pq.poll()[1];
+        }
+        return ans;
+
     }
 }
