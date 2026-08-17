bb7dea842bda11cd019f3979daec3507b320c8e2
Shivang <kshivang679@gmail.com>
2026-08-17 11:58:41 +0000

    Sync LeetCode submission - Sliding Window Maximum (java)

--- my-solutions/problems/sliding_window_maximum/solution.java
+++ my-solutions/problems/sliding_window_maximum/solution.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         Deque<Integer> q = new LinkedList<>();
-        
-        int n  = nums.length;
-        for(int i =0;i<n;i++){
-            if(!q.isEmpty() && q.getFirst()< i-k+1){
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (!q.isEmpty() && q.getFirst() < i - k + 1) {
                 q.pollFirst();
             }
-
-            while(!q.isEmpty() && nums[q.getLast()]< nums[i]){
+            while (!q.isEmpty() && nums[i] > nums[q.getLast()]) {
                 q.pollLast();
             }
             q.addLast(i);
             if(i>=k-1){
-                list.add(nums[q.getFirst()]);
+                ans.add(nums[q.getFirst()]);
             }
-            
         }
-        int [] ans = new int[list.size()];
-        for(int i=0 ;i<list.size();i++){
-            ans[i] = list.get(i);
+        int [] res = new int[ans.size()];
+        for(int i =0;i<ans.size();i++){
+            res[i] =ans.get(i);
         }
-        return ans;
+        return res;
     }
 }
