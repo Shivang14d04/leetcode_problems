a97ac01304f0a82e109299bf78793cbf5cc2a0d0
Shivang <kshivang679@gmail.com>
2026-08-19 06:26:45 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- my-solutions/problems/add_two_numbers/solution.java
+++ my-solutions/problems/add_two_numbers/solution.java
@@ -10,26 +10,29 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       ListNode dummy = new ListNode();
-       ListNode temp = dummy;
-       int carry =0;
-       while(l1!=null || l2!= null || carry == 1){
-        int sum = 0;
-        if(l1!= null){
-            sum += l1.val;
-            l1 = l1.next;
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if(l1!=null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+                        if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(sum%10);
+            curr  = curr.next;
+            carry = sum /10;
         }
-        if(l2!= null){
-            sum += l2.val;
-            l2 = l2.next;
+
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
         }
-        sum += carry;
-        carry = sum/10;
-        
-        ListNode node = new ListNode(sum%10);
-        temp.next = node;
-        temp = temp.next;
-       }
-       return dummy.next;
+        return dummy.next;
+
     }
 }
